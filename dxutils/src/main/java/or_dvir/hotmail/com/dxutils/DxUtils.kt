793e24ee9aa06a8db7e69be451c272959a38531f
Or Dvir <or_dvir@hotmail.com>
2021-05-22 19:14:33 +0200

    Update DxUtils.kt

--- dxutils/src/main/java/or_dvir/hotmail/com/dxutils/DxUtils.kt
+++ dxutils/src/main/java/or_dvir/hotmail/com/dxutils/DxUtils.kt
@@ -1,23 +1,157 @@
 package or_dvir.hotmail.com.dxutils
 
 import android.app.AlertDialog
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.support.annotation.StringRes
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import java.util.*
-import kotlin.collections.ArrayList
+import java.util.Locale
 import kotlin.math.roundToInt
 
 typealias simpleCallback = () -> Unit
+
+//region sms
+fun Context.sendSMS(number: String, @StringRes textRes: Int = -1): Boolean {
+    val text =
+        if (textRes == -1) {
+            ""
+        } else {
+            resources.getString(textRes)
+        }
+
+    return sendSMS(number, text)
+}
+
+fun Context.sendSMS(number: String, text: String = ""): Boolean {
+    return try {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("sms:$number"))
+        intent.putExtra("sms_body", text)
+        startActivity(intent)
+        true
+    } catch (e: Exception) {
+        e.printStackTrace()
+        false
+    }
+}
+//endregion
+
+fun Context.makeCall(number: String): Boolean {
+    return try {
+        val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:$number"))
+        startActivity(intent)
+        true
+    } catch (e: Exception) {
+        e.printStackTrace()
+        false
+    }
+}
+
+//region email
+fun Context.email(
+    email: String,
+    @StringRes subjectRes: Int = -1,
+    @StringRes textRes: Int = -1
+): Boolean {
+    val subject =
+        if (subjectRes == -1) {
+            ""
+        } else {
+            resources.getString(subjectRes)
+        }
+
+    val text =
+        if (textRes == -1) {
+            ""
+        } else {
+            resources.getString(textRes)
+        }
+
+    return email(email, subject, text)
+}
+
+fun Context.email(email: String, subject: String = "", text: String = ""): Boolean {
+    val intent = Intent(Intent.ACTION_SENDTO).apply {
+        data = Uri.parse("mailto:")
+        putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+
+        if (subject.isNotEmpty()) {
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+        }
+
+        if (text.isNotEmpty()) {
+            putExtra(Intent.EXTRA_TEXT, text)
+        }
+    }
+
+    if (intent.resolveActivity(packageManager) != null) {
+        startActivity(intent)
+        return true
+    }
+
+    return false
+}
+//endregion
+
+//region browse
+fun Context.browse(@StringRes urlRes: Int, newTask: Boolean = false) =
+    browse(resources.getString(urlRes), newTask)
+
+fun Context.browse(url: String, newTask: Boolean = false): Boolean {
+    return try {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(url)
+
+            if (newTask) {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+        }
+
+        startActivity(intent)
+        true
+    } catch (e: ActivityNotFoundException) {
+        e.printStackTrace()
+        false
+    }
+}
+//endregion
+
+//region share
+fun Context.share(@StringRes textRes: Int, @StringRes subjectRes: Int = -1): Boolean {
+    val text = resources.getString(textRes)
+    val subject =
+        if (subjectRes == -1) {
+            ""
+        } else {
+            resources.getString(subjectRes)
+        }
+
+    return share(text, subject)
+}
+
+fun Context.share(text: String, subject: String = ""): Boolean {
+    return try {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, text)
+        }
+
+        startActivity(Intent.createChooser(intent, null))
+        true
+    } catch (e: ActivityNotFoundException) {
+        e.printStackTrace()
+        false
+    }
+}
+//endregion
 
 inline fun <reified T : Any> AppCompatActivity.startActivityForResult(
     requestCode: Int,
